06855d981eb6eab14f9160a39a127dc727cbaffb
ruoskija <16821766+ruoskija@users.noreply.github.com>
2019-11-22 17:59:48 +0200

    Put flag parsing to its own function

--- main.go
+++ main.go
@@ -9,21 +9,29 @@ import (
 
 func main() {
 
-	rulePtr := flag.Int("rule", 30, "rule aka Wolfram code")
-	statePtr := flag.String("init", "0000000000000001000000000000000", "initial state of the automata")
-	stepsPtr := flag.Int("step", 16, "number of steps printed")
-	flag.Parse()
+	rule, steps, initialState := parseFlags()
 
-	system := automata.NewSystem(*rulePtr)
-	if err := system.SetState(*statePtr); err != nil {
+	system := automata.NewSystem(rule)
+	if err := system.SetState(initialState); err != nil {
 		fmt.Printf("Error: %v", err)
 		return
 	}
 
-	for i := 0; i < *stepsPtr; i++ {
+	for i := 0; i < steps; i++ {
 		fmt.Println(system.String())
 		system.Step()
 	}
 
 	return
+}
+
+func parseFlags() (int, int, string) {
+	rulePtr := flag.Int("rule", 30, "rule aka Wolfram code")
+	stepsPtr := flag.Int("step", 16, "number of steps printed")
+	statePtr := flag.String(
+		"init",
+		"0000000000000001000000000000000",
+		"initial state of the automata")
+	flag.Parse()
+	return *rulePtr, *stepsPtr, *statePtr
 }
